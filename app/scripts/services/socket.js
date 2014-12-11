54bad7852e0687e3cb9a532e0cc5037c4f2120f9
Conner Brooks <connerbrooks@gmail.com>
2014-12-11 01:25:20 -0500

    Adds socket service template. Preliminary THREE.js additions.

---
+++ app/scripts/services/socket.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webClientApp.socket
+ * @description
+ * # socket
+ * Service in the webClientApp.
+ */
+angular.module('webClientApp')
+  .service('socket', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
