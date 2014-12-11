6fc0dcde8119361b9802edf0e72d198e70fd9454
Conner Brooks <connerbrooks@gmail.com>
2014-12-11 01:41:37 -0500

    Adds prelinary socket code.

--- app/scripts/services/socket.js
+++ app/scripts/services/socket.js
@@ -9,5 +9,24 @@
  */
 angular.module('webClientApp')
   .service('socket', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+
+  var localWebsocket = new WebSocket("ws://localhost:8887/echo");
+  ws.onopen = function()
+  {
+     // Web Socket is connected, send data using send()
+    ws.send("We are connected");
+    console.log('local ws send: ');
+  };
+  ws.onmessage = function (evt)
+  {
+    var received_msg = evt.data;
+    console.log('local ws recv: ' + received_msg);
+  };
+  ws.onclose = function()
+  {
+    // websocket is closed.
+    console.log('local socket closed.');
+  };
+
+
   });
