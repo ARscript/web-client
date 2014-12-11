4af12a72abc1e87ac19a550cef107ccfce24ef38
Danny Grove <danny@drgrovellc.com>
2014-12-10 22:33:09 -0800

    Code Cleanup, Fixes ThreeJS
    * Adds Title
    * Fixes ThreeJS integration
    * Moves host localhost -> 0.0.0.0
    * Forces browser to open localhost:9000/

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,120 +9,127 @@ var $ = jQuery;
  * Controller of the webClientApp
  */
 angular.module('webClientApp')
-.controller('MainCtrl', function ($scope) {
-  $scope.data = {};
-  $scope.data.vimMode = true;
+  .controller
+  ( 'MainCtrl'
+  , function
+    ( $scope
+    )
+    {
+      $scope.data = {};
+      $scope.data.vimMode = true;
 
-  $scope.aceLoaded = function(_editor) {
-    // Options
-  };
-  $scope.aceChanged = function(e) {
-    //
-  };
+      $scope.aceLoaded = function(_editor) {
+        // Options
+      };
+      $scope.aceChanged = function(e) {
+        //
+      };
 
-  var videoSelect = document.querySelector('select#videoSource');
-  var videoElement = document.getElementById('camera-stream');
+      var videoSelect = document.querySelector('select#videoSource');
+      var videoElement = document.getElementById('camera-stream');
 
-  $scope.wsTest = function(){
-    if(!'WebSocket' in window) {
-      alert('WebSockets are not available in your browser. This app will not work');
-    }
-  };
+      $scope.wsTest = function(){
+        if(!'WebSocket' in window) {
+          alert('WebSockets are not available in your browser. This app will not work');
+        }
+      };
 
-  var updateEditorStyle = function() {
-    var windowHeight = $(window).height() - 50;
-    // $('.aceContainer').heigth(windowHeight + 'px');
-  };
+      var updateEditorStyle = function() {
+        var windowHeight = $(window).height() - 50;
+        // $('.aceContainer').heigth(windowHeight + 'px');
+      };
 
 
-  // Normalize the various vendor prefixed versions of getUserMedia.
-  navigator.getUserMedia = (navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia);
+      // Normalize the various vendor prefixed versions of getUserMedia.
+      navigator.getUserMedia = (navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia ||
+          navigator.msGetUserMedia);
 
-  function gotSources(sourceInfos) {
-    for (var i = 0; i !== sourceInfos.length; ++i) {
-      var sourceInfo = sourceInfos[i];
-      var option = document.createElement('option');
-      option.value = sourceInfo.id;
-      if (sourceInfo.kind === 'video') {
-        option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
-        videoSelect.appendChild(option);
-      } else {
-        console.log('Some other kind of source: ', sourceInfo);
+      function gotSources(sourceInfos) {
+        for (var i = 0; i !== sourceInfos.length; ++i) {
+          var sourceInfo = sourceInfos[i];
+          var option = document.createElement('option');
+          option.value = sourceInfo.id;
+          if (sourceInfo.kind === 'video') {
+            option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
+            videoSelect.appendChild(option);
+          } else {
+            console.log('Some other kind of source: ', sourceInfo);
+          }
+        }
       }
-    }
-  }
 
 
-  function start(){
-    if (!!window.stream) {
-      videoElement.src = null;
-      window.stream.stop();
-    }
-    var videoSource = videoSelect.value;
-    var constraints = {
-      video: {
-        optional: [{sourceId: videoSource}]
+      function start(){
+        if (!!window.stream) {
+          videoElement.src = null;
+          window.stream.stop();
+        }
+        var videoSource = videoSelect.value;
+        var constraints = {
+          video: {
+            optional: [{sourceId: videoSource}]
+          }
+        };
+        navigator.getUserMedia(constraints, successCallback, errorCallback);
       }
-    };
-    navigator.getUserMedia(constraints, successCallback, errorCallback);
-  }
 
-  function errorCallback(error){
-    console.log('navigator.getUserMedia error: ', error);
-  }
+      function errorCallback(error){
+        console.log('navigator.getUserMedia error: ', error);
+      }
 
-  function successCallback(stream) {
-    window.stream = stream; // make stream available to console
-    videoElement.src = window.URL.createObjectURL(stream);
-    videoElement.play();
-  }
+      function successCallback(stream) {
+        window.stream = stream; // make stream available to console
+        videoElement.src = window.URL.createObjectURL(stream);
+        videoElement.play();
+      }
 
-  MediaStreamTrack.getSources(gotSources);
-  videoSelect.onchange = start;
-  start();
+      MediaStreamTrack.getSources(gotSources);
+      videoSelect.onchange = start;
+      start();
 
-  // TODO make all k
-  init();
-  animate();
+      // TODO make all k
+      var camera, scene, renderer;
+      var geometry, material, mesh;
+      init();
+      animate();
 
-  function init() {
+      function init() {
 
-    var contentWidth = $("#video-container").width()
-    var contentHeight = $("#video-container").height()
-    camera = new THREE.PerspectiveCamera(75, contentWidth / contentHeight, 1, 10000);
-    camera.position.z = 1000;
+        var contentWidth = $("#video-container").width()
+        var contentHeight = $("#video-container").height()
+        camera = new THREE.PerspectiveCamera(75, contentWidth / contentHeight, 1, 10000);
+        camera.position.z = 1000;
 
-    scene = new THREE.Scene();
+        scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-             wireframe: true
-    });
+        geometry = new THREE.BoxGeometry(200, 200, 200);
+        material = new THREE.MeshBasicMaterial({
+          color: 0xff0000,
+                 wireframe: true
+        });
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+        mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
 
-    renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setSize(contentWidth, contentHeight);
+        renderer = new THREE.WebGLRenderer({alpha: true});
+        renderer.setSize(contentWidth, contentHeight);
 
-    $(renderer.domElement).prependTo("#video-container")
-    //document.getElementById('video-content').appendChild(renderer.domElement);
+        $(renderer.domElement).prependTo("#video-container")
 
-  }
+      }
 
-  function animate() {
+      function animate() {
 
-    requestAnimationFrame(animate);
+        requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
 
-    renderer.render(scene, camera);
+        renderer.render(scene, camera);
 
-  }
+      }
 
-});
+    }
+  );
